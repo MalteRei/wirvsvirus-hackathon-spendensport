32b2631f91f125e85f4bddd96e9ba7d33c785090
MalteRei <maltereimann@hotmail.de>
2020-03-23 23:01:02 +0100

    trying to fix cors error

--- frontend/spendensport/src/app/models/IConfig.ts
+++ frontend/spendensport/src/app/models/IConfig.ts
@@ -1,4 +1,5 @@
 export interface IConfig {
+    apiUrl: string;
     donationApiUrl: string;
 }
 
--- frontend/spendensport/src/app/models/config.ts
+++ frontend/spendensport/src/app/models/config.ts
@@ -3,6 +3,15 @@ import { IConfig } from './IConfig';
 export class Config implements IConfig {
     private DonationApiUrl: string;
 
+    private ApiUrl: string;
+
+    public get apiUrl(): string {
+        return this.ApiUrl;
+    }
+    public set apiUrl(value: string) {
+        this.ApiUrl = value;
+    }
+
     public get donationApiUrl(): string {
         return this.DonationApiUrl;
     }
