1a681e7210503de770f71e1844f0b6dd72b66272
kaisnb <kai.schoennerger95@googlemail.com>
2020-04-03 20:55:46 +0200

    Updated app title

--- frontend/spendensport/src/app/app.component.ts
+++ frontend/spendensport/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { GoalStoreService } from './services/goal-store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'spendensport';
-}
+export class AppComponent {}
 
 
